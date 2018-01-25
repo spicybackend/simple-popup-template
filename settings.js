e71cdb830f3dd24192586b29f42bbac6a9e19b79
Jordane Lew <jordane.lew@fluxfederation.com>
2018-01-25 14:37:08 +1300

    Work old webextension for intent

--- settings.js
+++ settings.js
@@ -1,30 +1,25 @@
-var webExtension = chrome || browser;
+let webExtension = chrome || browser;
 
-var emailEntry = document.getElementById('settings-email-entry');
-var passwordEntry = document.getElementById('settings-password-entry');
-var tokenEntry = document.getElementById('settings-token-entry');
-var autoLoginToggle = document.getElementById('settings-auto-login-toggle');
+let protocolEntry = document.getElementById('settings-protocol-entry');
+let directoryEntry = document.getElementById('settings-directory-entry');
 
 function saveSettings() {
   webExtension.storage.local.set({
-    email: emailEntry.value,
-    password: passwordEntry.value,
-    token: tokenEntry.value,
-    autoLogin: autoLoginToggle.checked
+    protocol: protocolEntry.value,
+    directory: directoryEntry.value
   });
 }
 
 function fillSettings() {
   webExtension.storage.local.get(function (settings) {
-    emailEntry.value = settings.email || '';
-    passwordEntry.value = settings.password || '';
-    tokenEntry.value = settings.token || '';
+    protocolEntry.value = settings.protocol || '';
+    directoryEntry.value = settings.directory || '';
 
-    if (settings.autoLogin) {
-      autoLoginToggle.setAttribute('checked', 'true');
-    } else {
-      autoLoginToggle.removeAttribute('checked');
-    }
+    // if (settings.autoLogin) {
+    //   autoLoginToggle.setAttribute('checked', 'true');
+    // } else {
+    //   autoLoginToggle.removeAttribute('checked');
+    // }
   });
 }
 
